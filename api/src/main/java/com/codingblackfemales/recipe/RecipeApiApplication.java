b20cdedfc2fad16797b8688ee6a367b5b24aec43
Solange Umuhire Gasengayire <sgasengayire@gmail.com>
2022-10-01 16:55:41 +0100

    Initialise the recipe api project (source)

---
+++ api/src/main/java/com/codingblackfemales/recipe/RecipeApiApplication.java
@@ -0,0 +1,13 @@
+package com.codingblackfemales.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeApiApplication.class, args);
+	}
+
+}
